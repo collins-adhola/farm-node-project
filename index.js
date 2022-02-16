be898a90a4039b43b0645403c89fcfa6506b5d20
Collino <coadhola@gmail.com>
2022-02-16 17:52:27 +0000

    final_ render to screen

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const replaceTemplate = require ('./modules/replaceTemplate')
+
 // Blocking, Synchronous
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -17,6 +19,21 @@ const url = require("url");
 // console.log('WHY SHOULD I GO')
 
 // Server
+
+//Now in replaceTemplate modele
+// const replaceTemplate = (temp, product) => {
+//   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+//   output = output.replace(/{%IMAGE%}/g, product.image);
+//   output = output.replace(/{%PRICE%}/g, product.price);
+//   output = output.replace(/{%FROM%}/g, product.from);
+//   output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+//   output = output.replace(/{%QUANTITY%}/g, product.quantity);
+//   output = output.replace(/{%DESCRIPTION%}/g, product.description);
+//   output = output.replace(/{%ID%}/g, product.id);
+
+//   if(!product.organic)output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+//   return output;
+// }
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
@@ -31,32 +48,42 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  
+  const {query, pathname} = url.parse(req.url, true)
+  
 
   // Overview page
 
-  if (pathName === '/' || pathName === '/overview'){
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
-    res.end(tempOverview);
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+    res.end(output);
 
     // Product page
-  } else if (pathName === '/product'){
-    res.end("This is the PRODUCT");
-      
-   // API  
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
-  } else if(pathName === '/api') {
-      res.writeHead(200,{'Content-type': 'application/json'})
-      res.end(data);
+    // API
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
 
-      // NOT FOUND
-  }else {
+    // NOT FOUND
+  } else {
     res.writeHead(404, {
-      'Content-type' : 'text/html',
-      'my-own-header': 'hello-world'
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
     });
-    res.end('<h1>Page not found</h1>')
+    res.end("<h1>Page not found</h1>");
   }
 });
 server.listen(8000, "127.0.0.1", () =>
